2de43e5b123f8d4696cc85adc652b55bc2a557bb
Abner Souza <100322732+AbnerSouza2@users.noreply.github.com>
2025-02-19 17:56:53 -0300

    Alterado a estrutura do Controller

--- controllers/ContatoController.php
+++ controllers/ContatoController.php
@@ -1,45 +1,31 @@
 <?php
-require_once '../config/database.php';
+require_once '../models/Contato.php';
 
 class ContatoController {
-    private $conn;
+    private $contatoModel;
 
     public function __construct() {
-        $this->conn = Database::getConnection();
+        $this->contatoModel = new Contato(); 
     }
 
-    // Método para listar todos os contatos.
+    // Listar todos os contatos
     public function index() {
-        $stmt = $this->conn->prepare("SELECT id, nome, data_nascimento, email, celular FROM contatos ORDER BY id DESC");
-        $stmt->execute();
-        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        return $this->contatoModel->listar(); 
     }
 
-    // Método para adicionar um novo contato
-    public function store($dados) {
-        $stmt = $this->conn->prepare("INSERT INTO contatos (nome, email, telefone, celular, data_nascimento, profissao, whatsapp, email_notificacao, sms_notificacao) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)");
-        $stmt->execute([
-            $dados['nome'], $dados['email'], $dados['telefone'], $dados['celular'],
-            $dados['data_nascimento'], $dados['profissao'], isset($dados['whatsapp']) ? 1 : 0,
-            isset($dados['email_notificacao']) ? 1 : 0, isset($dados['sms_notificacao']) ? 1 : 0
-        ]);
+    // Adicionar um novo contato
+    public function salvar_cadastro($dados) {
+        $this->contatoModel->salvar($dados); 
     }
 
-    // Método para atualizar um cadastro existente
-    public function update($dados) {
-        $stmt = $this->conn->prepare("UPDATE contatos SET nome=?, email=?, telefone=?, celular=?, data_nascimento=?, profissao=?, whatsapp=?, email_notificacao=?, sms_notificacao=? WHERE id=?");
-        $stmt->execute([
-            $dados['nome'], $dados['email'], $dados['telefone'], $dados['celular'],
-            $dados['data_nascimento'], $dados['profissao'], isset($dados['whatsapp']) ? 1 : 0,
-            isset($dados['email_notificacao']) ? 1 : 0, isset($dados['sms_notificacao']) ? 1 : 0,
-            $dados['id']
-        ]);
+    // Atualizar um contato existente
+    public function atualizar_cadastro($dados) {
+        return $this->contatoModel->atualizar($dados); 
     }
 
-    // Método para excluir algum cadastro
-    public function destroy($id) {
-        $stmt = $this->conn->prepare("DELETE FROM contatos WHERE id=?");
-        $stmt->execute([$id]);
+    // Excluir um contato
+    public function excluir_cadastro($id) {
+        return $this->contatoModel->excluir($id); 
     }
 }
 ?>
